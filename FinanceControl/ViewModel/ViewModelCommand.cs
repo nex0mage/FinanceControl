985288c32b47fa9ead1b227e009921575f08d855
trace <trace@DESKTOP-TC3U6ED>
2024-01-21 22:51:28 +0300

    Добавьте файлы проекта.

---
+++ FinanceControl/ViewModel/ViewModelCommand.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace FinanceControl.ViewModel
+{
+    public class ViewModelCommand : ICommand
+    {
+        // Поля
+        private readonly Action<object> _executeAction;
+        private readonly Predicate<object> _canExecuteAction;
+
+        //Конструкторы
+        public ViewModelCommand(Action<object> executeAction, Predicate<object> canExecuteAction)
+        {
+            _executeAction = executeAction;
+            _canExecuteAction = canExecuteAction;
+        }
+
+        public ViewModelCommand(Action<object> executeAction)
+        {
+            _executeAction = executeAction;
+        }
+
+        //События
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+
+        }
+
+
+        //Методы
+        public bool CanExecute(object parameter)
+        {
+            return _canExecuteAction == null ? true : _canExecuteAction(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            _executeAction(parameter);
+        }
+
+    }
+}
